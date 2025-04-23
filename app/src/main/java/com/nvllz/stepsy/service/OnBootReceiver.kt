4cc4ef6e6648ea97629557a54c0905e8623cd345
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-04-23 15:18:58 -0700

    feat(OnBootReceiver): remove SDK version check
    Removes unnecessary SDK check since minimum project SDK version is 26.

--- app/src/main/java/com/nvllz/stepsy/service/OnBootReceiver.kt
+++ app/src/main/java/com/nvllz/stepsy/service/OnBootReceiver.kt
@@ -17,12 +17,8 @@ import android.os.Build
  */
 internal class OnBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action?.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true) == true) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(Intent(context, MotionService::class.java))
-            } else {
-                context.startService(Intent(context, MotionService::class.java))
-            }
-        }
+        if (intent.action?.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true) != true) return
+
+        context.startForegroundService(Intent(context, MotionService::class.java))
     }
 }
